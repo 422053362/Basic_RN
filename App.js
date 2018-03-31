8b20f1f555c66ea39a6488320703f4354d01b1f9
gurdipsingh <gurdipsingh1990@gmail.com>
2018-03-31 15:46:42 -0700

    Started android emulator wih hot reload

--- App.js
+++ App.js
@@ -5,7 +5,7 @@ export default class App extends React.Component {
   render() {
     return (
       <View style={styles.container}>
-        <Text>Testing......</Text>
+        <Text>Testing simeple...</Text>
       </View>
     );
   }

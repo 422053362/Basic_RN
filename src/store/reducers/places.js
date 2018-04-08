713526c498c2fd2264c404473957fabab443dfe3
gurdipsingh <gurdipsingh1990@gmail.com>
2018-04-08 15:24:10 -0700

    Initialzes reducer file with initial empty state, actions coming next

---
+++ src/store/reducers/places.js
@@ -0,0 +1,14 @@
+const initialState = {
+  places: [],
+  selectedPlace: null
+}
+
+const reducer = (state = initialState, action) => {
+  switch (action.type) {
+
+    default:
+      state;
+  }
+}
+
+export defaul reducer;

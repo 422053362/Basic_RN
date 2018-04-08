5669c64db0792745de3e282816f7f254c9738091
gurdipsingh <gurdipsingh1990@gmail.com>
2018-04-08 15:38:11 -0700

    added reducer to handle add_place case, importing image directly from assets rather than web url

--- src/store/reducers/places.js
+++ src/store/reducers/places.js
@@ -1,3 +1,11 @@
+import {
+  ADD_PLACE,
+  SELECT_PLACE,
+  DESELECT_PLACE,
+  DELETE_PLACE
+} from '../actions/actionTypes';
+import placeImage from '../../../src/assets/blue-german-shepherd-pictures-1.jpg';
+
 const initialState = {
   places: [],
   selectedPlace: null
@@ -5,7 +13,14 @@ const initialState = {
 
 const reducer = (state = initialState, action) => {
   switch (action.type) {
-
+    case ADD_PLACE:
+      return {
+        ...state,
+        places: prevState.places.concat({
+          key: Math.random(),
+          name: action.placeName,
+          image: placeImage
+      };
     default:
       state;
   }

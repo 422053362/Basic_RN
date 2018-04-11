645fb82c9b937cb3c3d97377c79ae990e1ef1994
gurdipsingh <gurdipsingh1990@gmail.com>
2018-04-11 15:41:03 -0700

    Added sharePlace and findPlace screens to display after use is authenticated

---
+++ src/screens/FindPlace/FindPlace.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+import {
+  View,
+  Text
+} from 'react-native';
+
+class FindPlace extends Component {
+  render() {
+    return (
+      <View>
+        <Text>On FindPlace screen</Text>
+      </View>
+    );
+  }
+}
+
+export default FindPlace;
---
+++ src/screens/SharePlace/SharePlace.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+import {
+  View,
+  Text
+} from 'react-native';
+
+class SharePlace extends Component {
+  render() {
+    return (
+      <View>
+        <Text>On SharePlace screen</Text>
+      </View>
+    );
+  }
+}
+
+export default SharePlace;
